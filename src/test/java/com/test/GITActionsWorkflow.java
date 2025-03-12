99997e26239e0f0c435201254a82a5de31c37cde
Ramkumar S <ramkumars@Ramkumars-MacBook-Air.local>
2025-03-12 10:12:29 +0530

    first code

---
+++ src/test/java/com/test/GITActionsWorkflow.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class GITActionsWorkflow {
+
+	@Test
+	public void sampleTest() {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
